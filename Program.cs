77177818a76740b505aa6cc33505109424d66cd5
Nikolay <nikshowsix@gmail.com>
2023-12-20 21:02:57 +0300

    prog

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿Урок 1. Контрольная работа
+﻿/* Урок 1. Контрольная работа
 
 Для выполнения проверочной работы необходимо:
 
@@ -7,3 +7,50 @@
 3. Снабдить репозиторий оформленным текстовым описанием решения (файл README.md)
 4. Написать программу, решающую поставленную задачу
 5. Использовать контроль версий в работе над этим небольшим проектом (не должно быть так, что всё залито одним коммитом, как минимум этапы 2, 3, и 4 должны быть расположены в разных коммитах)
+*/
+
+string[] CreateArray()
+{
+    Console.Write("Введите размер массива: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    string[] array = new string[size];
+    for (int i = 0; i < size; i += 1)
+    {
+        Console.Write($"Введите {i + 1} элемент массива: ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i += 1)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+string[] ResultArray(string[] array)
+{
+    int c = 0;
+    int j = 0;
+    for (int i = 0; i < array.Length; i += 1)
+    {
+        if (array[i].Length <= 3) c += 1;
+    }
+    string[] arrayRes = new string[c];
+    for (int i = 0; i < array.Length; i += 1)
+     {
+        if (array[i].Length <= 3)
+        {
+            arrayRes[j] = array[i];
+            j += 1;
+        }
+    }
+    return arrayRes;
+}
+string[] arrayFaсt = CreateArray();
+Console.WriteLine();
+Console.WriteLine("Созданный массив:");
+ShowArray(arrayFaсt);
+Console.WriteLine();
+string[] arrayRes = ResultArray(arrayFaсt);
+Console.WriteLine("Новый массив из строк, длина которых меньше, либо равна 3 символам:");
+ShowArray(arrayRes);
